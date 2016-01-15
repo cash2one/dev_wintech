9047d297547c47905927d2aaca8fce6cee2f3a2e
litwrd <litwrd@163.com>
2016-01-15 15:42:52 +0800

    update utils http

--- v0000/win_utils/UtilsHttp.pas
+++ v0000/win_utils/UtilsHttp.pas
@@ -2,17 +2,44 @@ unit UtilsHttp;
 
 interface
 
+uses
+  Windows, ActiveX, UrlMon;
+
   function GetHttpUrlData(AUrl: string): string; overload;      
   function GetHttpUrlData(AUrl: string; APost: string): string; overload;
   function GetHttpUrlFile(AUrl: string; AOutputFile: string): Boolean; overload;
 
-  function PostHttpUrlData(AUrl: string; APost: string): string;       
+  function PostHttpUrlData(AUrl: string; APost: string): string;     
+
+  function GetDefaultUserAgent: string;  
 
 implementation
 
 uses
   UtilsHttp_Indy;
 
+function GetDefaultUserAgent: string;
+var
+  s: AnsiString;
+  cbSize: Cardinal;
+  hRet: HRESULT;
+begin
+  cbSize := MAX_PATH;
+  repeat
+    SetLength(s, cbSize);
+    hRet := UrlMon.ObtainUserAgentString(0, PAnsiChar(s), cbSize);
+    case hRet of
+      E_OUTOFMEMORY:
+        cbSize := cbSize * 2;
+      NOERROR:
+        SetLength(s, cbSize - 1);
+      else
+        SetLength(s, 0);
+    end;
+  until hRet <> E_OUTOFMEMORY;
+  Result := string(s);
+end;
+  
 function GetHttpUrlData(AUrl: string): string;
 begin
   //Result := Http_WinInet.Http_GetString(AUrl);     
