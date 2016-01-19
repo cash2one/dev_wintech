004f51556216af9fe4a03bb05874b504352817f3
litwrd <litwrd@163.com>
2016-01-19 18:20:02 +0800

    update thread

--- v0000/win_utils/UtilsWinThread.pas
+++ v0000/win_utils/UtilsWinThread.pas
@@ -2,6 +2,7 @@ unit UtilsWinThread;
 
 interface
 
+  { 无锁编程以及CAS }
   function CAS32(AOldValue, ANewValue: Pointer; ADestination: Pointer): Boolean; overload;  
   function CAS32(AOldValue, ANewValue: Cardinal; ADestination: Pointer): Boolean; overload;
   function CAS32(AOldValue, ANewValue: Integer; ADestination: Pointer): boolean;overload;
@@ -12,6 +13,23 @@ interface
 
 implementation
 
+uses
+  Windows;
+  
+(*
+http://www.cnblogs.com/caca/p/lock-free_CAS_ABA.html
+http://www.360doc.com/content/14/0811/10/1073512_400983810.shtml
+使用lock实现线程同步有很多缺点：
+  * 产生竞争时，线程被阻塞等待，无法做到线程实时响应。
+  * dead lock。
+  * live lock。
+  * 优先级翻转。
+  * 使用不当，造成性能下降。
+  如果在不使用 lock 的情况下 实现变量同步
+  那就会避免很多问题 虽然目前来看 无锁编程并不能替代 lock
+
+  非同步阻塞的实现可以分成三个级别：wait-free/lock-free/obstruction-free
+*)
 ////////////////////////////////////////////////////////////////////////////////
 //Assembly functions
 function CAS32(AOldValue, ANewValue: Pointer; ADestination: Pointer): Boolean; overload;
@@ -124,5 +142,14 @@ asm
 {$ENDIF CPUX64}
   setz  al
 end; { CAS }
+
+procedure WinThreadLockCAS();
+begin
+  //Windows.InterlockedCompareExchange(
+    //var Destination: Longint;
+    //Exchange: Longint;
+    //Comperand: Longint
+  //);
+end;
 
 end.
