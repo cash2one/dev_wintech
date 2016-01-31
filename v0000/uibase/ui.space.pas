082fabd8ecf5895787ccd5916945adcac778afe0
litwrd <litwrd@163.com>
2016-01-31 22:28:02 +0800

    update ui.space

--- v0000/uibase/ui.space.pas
+++ v0000/uibase/ui.space.pas
@@ -17,6 +17,19 @@ type
     X           : TFloat;
     Y           : TFloat;
   end;
+         
+  PUIRect       = Types.PRect;
+  TUIRect       = Types.TRect;
+          
+  PUIFloatRect  = ^TUIFloatRect;
+  TUIFloatRect  = packed record
+    case Integer of
+      0: (Left, Top, Right, Bottom: TFloat);
+      1: (TopLeft, BottomRight: TUIFloatPoint);
+  end;
+          
+const
+  ZERO_RECT: TUIRect = (Left: 0; Top: 0; Right: 0; Bottom: 0);
 
 implementation
 
