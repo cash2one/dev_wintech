2075d07b0319db3c1b74a94adc40c42c90cfd92e
litwrd <litwrd@163.com>
2016-02-03 10:33:23 +0800

    update utils http

--- v0000/win_utils/UtilsHttp.pas
+++ v0000/win_utils/UtilsHttp.pas
@@ -5,20 +5,40 @@ interface
 uses
   Windows, ActiveX, UrlMon;
 
-  function GetHttpUrlData(AUrl: string): string; overload;      
-  function GetHttpUrlData(AUrl: string; APost: string): string; overload;
-  function GetHttpUrlFile(AUrl: string; AOutputFile: string): Boolean; overload;
+type
+  PNetClientSession = ^TNetClientSession;
+  TNetClientSession = record
+    Connection: Pointer;
+  end;
 
-  function PostHttpUrlData(AUrl: string; APost: string): string;     
+  function CheckOutNetClientSession: PNetClientSession;
+  procedure CheckInNetClientSession(var ANetClientSession: PNetClientSession);
 
-  function GetDefaultUserAgent: string;  
+  function GetHttpUrlData(AUrl: AnsiString; ANetSession: PNetClientSession): string; overload;      
+  function GetHttpUrlData(AUrl: AnsiString; APost: AnsiString; ANetSession: PNetClientSession): string; overload;
+  function GetHttpUrlFile(AUrl: AnsiString; AOutputFile: AnsiString; ANetSession: PNetClientSession): Boolean; overload;
+
+  function PostHttpUrlData(AUrl: AnsiString; APost: AnsiString; ANetSession: PNetClientSession): string;
+
+  function GetDefaultUserAgent: AnsiString;  
 
 implementation
 
 uses
   UtilsHttp_Indy;
 
-function GetDefaultUserAgent: string;
+function CheckOutNetClientSession: PNetClientSession;
+begin
+  Result := System.New(PNetClientSession);
+  FillChar(Result^, SizeOf(TNetClientSession), 0);
+end;
+
+procedure CheckInNetClientSession(var ANetClientSession: PNetClientSession);
+begin
+
+end;
+
+function GetDefaultUserAgent: AnsiString;
 var
   s: AnsiString;
   cbSize: Cardinal;
@@ -37,27 +57,47 @@ begin
         SetLength(s, 0);
     end;
   until hRet <> E_OUTOFMEMORY;
-  Result := string(s);
+  Result := s;
 end;
   
-function GetHttpUrlData(AUrl: string): string;
-begin
-  //Result := Http_WinInet.Http_GetString(AUrl);     
-  Result := UtilsHttp_Indy.Http_GetString(AUrl);
-end;
-
-function GetHttpUrlFile(AUrl: string; AOutputFile: string): Boolean;
+function GetHttpUrlData(AUrl: string; ANetSession: PNetClientSession): string;
+var
+  tmpIndyConnection: PIndyConnection;
 begin    
-  Result := UtilsHttp_Indy.Http_GetFile(AUrl, AOutputFile);
+  tmpIndyConnection := nil;
+  if nil <> ANetSession then
+  begin
+    if nil = ANetSession.Connection then
+    begin
+      ANetSession.Connection := CheckOutIndyConnection;
+    end;
+    tmpIndyConnection := ANetSession.Connection;
+  end;
+  //Result := Http_WinInet.Http_GetString(AUrl);
+  Result := UtilsHttp_Indy.Http_GetString(AUrl, tmpIndyConnection);
 end;
 
-function GetHttpUrlData(AUrl: string; APost: string): string;
+function GetHttpUrlFile(AUrl: AnsiString; AOutputFile: AnsiString; ANetSession: PNetClientSession): Boolean;
+var
+  tmpIndyConnection: PIndyConnection;
+begin    
+  tmpIndyConnection := nil;     
+  if nil <> ANetSession then
+  begin
+    if nil = ANetSession.Connection then
+    begin
+    end;
+  end;
+  Result := UtilsHttp_Indy.Http_GetFile(AUrl, AOutputFile, tmpIndyConnection);
+end;
+
+function GetHttpUrlData(AUrl: AnsiString; APost: AnsiString; ANetSession: PNetClientSession): string;
 begin          
   Result := '';
 //  Result := Http_WinInet.Http_GetString(AUrl);
 end;
 
-function PostHttpUrlData(AUrl: string; APost: string): string;
+function PostHttpUrlData(AUrl: AnsiString; APost: AnsiString; ANetSession: PNetClientSession): string;
 begin
   Result := '';
 //  Result := Http_WinInet.Http_GetString(AUrl);
--- v0000/win_utils/UtilsHttp_Indy.pas
+++ v0000/win_utils/UtilsHttp_Indy.pas
@@ -1,28 +1,62 @@
 unit UtilsHttp_Indy;
 
 interface
-    
-  function Http_GetString(AURL: string): string;
-  function Http_GetFile(AUrl, AOutputFile: string): Boolean;
+
+type
+  PIndyConnection = ^TIndyConnection;
+  TIndyConnection = record
+    ConnectionObj : TObject;
+  end;
+
+  function CheckOutIndyConnection: PIndyConnection;   
+  procedure CheckInIndyConnection(var AIndyConnection: PIndyConnection);
+  
+  function Http_GetString(AURL: AnsiString; AIndyConnection: PIndyConnection): string;
+  function Http_GetFile(AUrl, AOutputFile: AnsiString; AIndyConnection: PIndyConnection): Boolean;
 
 implementation
 
 uses
   Classes, Sysutils, IdHttp;
 
-function Http_GetString(AURL: string): string;
+function CheckOutIndyConnection: PIndyConnection;
+begin
+  Result := System.New(PIndyConnection);
+  FillChar(Result^, SizeOf(TIndyConnection), 0);
+  Result.ConnectionObj := TIdHttp.Create(nil);
+end;
+
+procedure CheckInIndyConnection(var AIndyConnection: PIndyConnection);
+begin
+end;
+
+function Http_GetString(AURL: AnsiString; AIndyConnection: PIndyConnection): string;
 var
   tmpHttp: TIdHttp;
-begin             
-  tmpHttp := TIdHttp.Create(nil);
+  tmpIsOwned: Boolean;
+begin
+  tmpHttp := nil;
+  tmpIsOwned := false;
+  if nil <> AIndyConnection then
+  begin
+    tmpHttp := TIdHttp(AIndyConnection.ConnectionObj);
+  end;
+  if nil = tmpHttp then
+  begin
+    tmpIsOwned := true;
+    tmpHttp := TIdHttp.Create(nil);
+  end;
   try
     Result := tmpHttp.get(AURL);
   finally
-    tmpHttp.Free;
+    if tmpIsOwned then
+    begin
+      tmpHttp.Free;
+    end;
   end;
 end;
 
-function Http_GetFile(AUrl, AOutputFile: string): Boolean; 
+function Http_GetFile(AUrl, AOutputFile: AnsiString; AIndyConnection: PIndyConnection): Boolean; 
 var
   tmpHttp: TIdHttp;
   tmpOutputFile: TFileStream;
