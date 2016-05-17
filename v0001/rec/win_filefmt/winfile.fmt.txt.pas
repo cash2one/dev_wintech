ad3e5b1367113fe27bfb9870d87af099bccc1cb2
litwrd <litwrd@github.com>
2016-05-17 18:23:35 +0800

    update file format text

--- v0001/rec/win_filefmt/winfile.fmt.txt.pas
+++ v0001/rec/win_filefmt/winfile.fmt.txt.pas
@@ -2,6 +2,15 @@ unit winfile.fmt.txt;
 
 interface
 
+uses
+  win.diskfile;
+  
+type
+  TFileFmt_Txt    = record
+    WinFile       : PWinFile;
+    CodePage      : Integer;  // CP_UTF8    
+  end;
+  
 implementation
 
 end.
