0b8d0d88e5ba9349449d3bae50d85d542355c1b4
litwrd <litwrd@github.com>
2016-06-15 14:11:44 +0800

    update win.process

--- v0001/rec/win_sys/win.process.pas
+++ v0001/rec/win_sys/win.process.pas
@@ -2,6 +2,78 @@ unit win.process;
 
 interface
 
+uses
+  Windows;
+               
+type
+  POwnedProcess   = ^TOwnedProcess;
+  PExProcess      = ^TExProcess;
+  PRunProcess     = ^TRunProcess;
+  PCoreProcess    = ^TCoreProcess;
+  
+  TCoreProcess    = record
+    ProcessHandle : THandle;
+    ProcessId     : DWORD;
+  end;
+  
+  { 自身运行进程控制 }
+  TOwnedProcess   = record
+    Core          : TCoreProcess;
+    AppCmdWnd     : HWND;
+    Run           : PRunProcess;
+  end;
+
+  { 外部进程控制 }
+  TExProcess      = record
+    Core          : TCoreProcess;
+  end;
+
+  TRunProcess     = record
+    ProcessInfo   : TProcessInformation;
+    StartInfoA    : TStartupInfoA;
+    StartInfoW    : TStartupInfoW;
+  end;
+
+  procedure RunProcessA(AProcess: POwnedProcess; AExeFileUrl: AnsiString; ARunProcess: PRunProcess = nil);
+
 implementation
+
+procedure RunProcessA(AProcess: POwnedProcess; AExeFileUrl: AnsiString; ARunProcess: PRunProcess = nil);
+var
+  tmpIsOwnedRun: Boolean;
+begin
+  tmpIsOwnedRun := false;
+  AProcess.Run := ARunProcess;
+  if nil = AProcess.Run then
+  begin
+    tmpIsOwnedRun := true;
+    AProcess.Run := System.New(PRunProcess);
+    FillChar(AProcess.Run^, SizeOf(TRunProcess), 0);
+    AProcess.Run.StartInfoA.cb := SizeOf(AProcess.Run.StartInfoA);
+    AProcess.Run.StartInfoW.cb := SizeOf(AProcess.Run.StartInfoW);    
+  end;
+  if Windows.CreateProcessA(
+      PAnsiChar(AExeFileUrl), //lpApplicationName: PAnsiChar;
+      nil, //lpCommandLine: PAnsiChar;
+      nil, //lpProcessAttributes,
+      nil, //lpThreadAttributes: PSecurityAttributes;
+      false, //bInheritHandles: BOOL;
+      CREATE_NEW, //dwCreationFlags: DWORD;
+      nil, //lpEnvironment: Pointer;
+      nil, //lpCurrentDirectory: PAnsiChar;
+      AProcess.Run.StartInfoA, //const lpStartupInfo: TStartupInfoA;
+      AProcess.Run.ProcessInfo) //var lpProcessInformation: TProcessInformation
+      then
+  begin
+    AProcess.Core.ProcessHandle := AProcess.Run.ProcessInfo.hProcess;
+    AProcess.Core.ProcessId := AProcess.Run.ProcessInfo.dwProcessId;
+  end;
+  if tmpIsOwnedRun then
+  begin                   
+    FillChar(AProcess.Run^, SizeOf(TRunProcess), 0);
+    FreeMem(AProcess.Run);
+    AProcess.Run := nil;
+  end;
+end;
 
 end.
